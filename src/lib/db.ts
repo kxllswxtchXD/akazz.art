e0e9bce78b0e521cfc064128f2119585e9a43b1d
ZyRox <nuitsukera@akazz.art>
2025-01-19 17:16:11 -0300

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,11 @@
-// src/lib/db.ts
 import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+// Desabilita o erro para a próxima linha
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const query = (text: string, params?: any[]) => {
   return pool.query(text, params);
 };
