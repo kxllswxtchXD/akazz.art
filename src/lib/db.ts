04fa154acc93e2fcaabd13a54fb241f9366d4f58
ZyRox <nuitsukera@akazz.art>
2025-01-19 16:48:29 -0300

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,20 +1,7 @@
-// src/lib/db.ts
 import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
 });
 
-export const query = async (text: string, params: any[] = []) => {
-  const client = await pool.connect();
-  try {
-    const result = await client.query(text, params);
-    return result;
-  } catch (err) {
-    console.error('Erro na consulta SQL:', err);
-    throw err;
-  } finally {
-    client.release();
-  }
-};
+export default pool;
